ae9c4b9e0cdf974be54fe8e5e5db1283a268a5c7
Youssef Messaoudi <Youssef.compta@gmail.com>
2019-01-16 05:16:17 +0100

    My first commit

---
+++ app/src/main/java/com/youssefinfo/filmzapp/Controllers/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.youssefinfo.filmzapp.Controllers
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.youssefinfo.filmzapp.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
